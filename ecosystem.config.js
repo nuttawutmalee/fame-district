7fc11f317cc9fef98d08ac58bfce1ae5d38ff015
Nuttawut Malee <nmalee@palo-it.com>
2019-01-15 11:14:47 +0700

    comment out production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,14 +6,29 @@ module.exports = {
     },
   ],
   deploy: {
-    production: {
+    staging: {
       user: 'ubuntu',
       host: 'ec2-3-0-184-237.ap-southeast-1.compute.amazonaws.com',
       key: '~/.ssh/ec2.pem',
       ref: 'origin/master',
       repo: 'git@github.com:nuttawutmalee/wink-hotels.git',
-      path: '/home/ubuntu/gatsby-prismic',
-      'post-deploy': 'yarn install && npm run restart',
+      path: '/home/ubuntu/gatsby-prismic-staging',
+      'post-deploy': 'npm install && npm run serve --env dev',
+      env: {
+        NODE_ENV: 'development',
+      },
     },
+    // production: {
+    //   user: 'ubuntu',
+    //   host: 'ec2-3-0-184-237.ap-southeast-1.compute.amazonaws.com',
+    //   key: '~/.ssh/ec2.pem',
+    //   ref: 'origin/master',
+    //   repo: 'git@github.com:nuttawutmalee/wink-hotels.git',
+    //   path: '/home/ubuntu/gatsby-prismic',
+    //   'post-deploy': 'npm install && npm run serve --env production',
+    //   env: {
+    //     NODE_ENV: 'production',
+    //   },
+    // },
   },
 };
